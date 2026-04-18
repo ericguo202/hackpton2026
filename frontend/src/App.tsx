e24e8da989cffb7a6c46b198e5e4ed42541a0204
eric <eric.guo171@gmail.com>
2026-04-17 21:14:49 -0400

    added clerk auth

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,19 @@
 import './App.css'
+import { Show, UserButton } from '@clerk/react'
+import SignInPage from "./pages/SignInPage";
 
 function App() {
-
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <Show when="signed-out">
+        <SignInPage />
+      </Show>
+      <Show when="signed-in">
+        <div className="p-4">
+          <UserButton />
+          <h1 className="text-3xl font-bold">Signed in</h1>
+        </div>
+      </Show>
     </>
   )
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { ClerkProvider } from '@clerk/react';
 import App from './App.tsx'
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing VITE_CLERK_PUBLISHABLE_KEY in environment')
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <App />
+    </ClerkProvider>
   </StrictMode>,
 )
